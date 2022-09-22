332e2ec41ecc707d6c795935857ae565a959aa24
Omar Sherif <omarsherif74@hotmail.com>
2022-09-22 19:44:43 +0200

    added column in migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,15 +14,12 @@ return new class extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id('user_id');
-//            $table->integer('user_id');
             $table->string('name');
             $table->string('email')->unique();
-//            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('blood_type');
             $table->integer('age');
             $table->char('gender', '1');
-//            $table->rememberToken();
         });
     }
 
